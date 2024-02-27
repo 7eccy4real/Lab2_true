1d6849b2315e760120f81cc098c09127ed2fa704
Студент <@364-02>
2024-02-27 17:01:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+class Matrix{
+private:
+    unsigned int m;
+    unsigned int n;
+    vector<vector<int>> data;
+public:
+    Matrix(unsigned int m, unsigned int n) :data(m, vector<int>(n, 0)), m(m), n(n) {}
+    ~Matrix() {}
+    void fillRandom() {
+        srand(time(0));
+        for (unsigned int i = 0; i < m; ++i) {
+            for (unsigned int j = 0; j < n; ++j) {
+                data[i][j] = rand() % 10;
+            }
+        }
+    }
+};
+int main()
+{
+    Matrix a(3, 3);
+
+    return 0;
+}
